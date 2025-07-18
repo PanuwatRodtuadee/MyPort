7d534899d686406a3067a093bdaad7c6c64c9192
PanuwatRodtuadee <bhlg1644.bike@gmail.com>
2025-07-18 08:19:38 +0700

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<!-- src/routes/+layout.svelte -->
+<header>
+    <nav class = "menu">
+        <ul>
+            <h1>My Portfolio</h1>
+        </ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/performance">Performance</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </nav>
+    <nav class = "logo">
+            
+    </nav>
+</header>
+<main>
+    <slot></slot>
+</main>
